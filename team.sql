5c0d8ba0556cd1be41a675d2fc2c676e4c926565
iqingba <iqingba@126.com>
2024-02-22 07:58:28 +0800

    Update team.sql

--- team.sql
+++ team.sql
@@ -30,8 +30,8 @@ CREATE TABLE `team`  (
 -- ----------------------------
 -- Records of team
 -- ----------------------------
-INSERT INTO `team` VALUES (1001, '底特律活塞');
-INSERT INTO `team` VALUES (1002, '印第安纳步行者');
-INSERT INTO `team` VALUES (1003, '亚特兰大老鹰');
+INSERT INTO `team` VALUES (1001, 'Detroit Pistons');
+INSERT INTO `team` VALUES (1002, 'Indiana Pacers');
+INSERT INTO `team` VALUES (1003, 'Atlanta Hawks');
 
 SET FOREIGN_KEY_CHECKS = 1;
